eaf9f51300f6220926d61d017cd21a1533088ef7
Kaushlendra Pratap <kaushlendra.pratap@corpseed.com>
2025-09-06 18:13:25 +0530

    role API has been completed

--- src/main/java/com/doc/dto/role/RoleRequestDto.java
+++ src/main/java/com/doc/dto/role/RoleRequestDto.java
@@ -1,12 +1,16 @@
 package com.doc.dto.role;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 public class RoleRequestDto {
+
+    @NotNull(message = "Role ID cannot be null")
+    private Long id;
 
     @NotBlank(message = "Role name is mandatory")
     private String name;
