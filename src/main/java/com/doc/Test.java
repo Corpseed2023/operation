6b157ef4b70c12a53ad2eb8714dd3d8936944ad9
Kaushlendra Pratap <kaushlendra.pratap@corpseed.com>
2025-10-03 18:33:32 +0530

    project creation API has been added

--- src/main/java/com/doc/Test.java
+++ src/main/java/com/doc/Test.java
@@ -1,29 +1,36 @@
-//package com.doc;
-//
-//import java.util.*;
-//
-//public class Test {
-//
-//    public static void main(String[] args) {
-//
-//        String [] strs = {"flower","flow","flight"};
-//
-//        String prefix = strs[0];
-//
-//        for (int i = 1; i<strs.length;i++)
-//        {
-//
-//            while (!prefix.startsWith(strs[i]))
-//            {
-//                prefix =
-//
-//            }
-//        }
-//
-//
-//
-//    }
-//
-//
-//
-//}
+package com.doc;
+
+import java.util.*;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+
+        int [] nums = {0,0,0,1,2,2,4,4,6,6,7};
+
+        Map<Integer,Integer> map = new HashMap<>();
+        List<Integer> result = new ArrayList<>();
+
+
+        for (int i : nums)
+        {
+            if (!map.containsKey(i))
+            {
+                map.put(i,1);
+                result.add(i);
+            }
+        }
+        for (int num : result) {
+            System.out.println(num + " appears " + map.get(num) + " times");
+        }
+
+
+
+
+
+    }
+
+
+
+}
