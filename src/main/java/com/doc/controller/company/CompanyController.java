67410c2fb9f44af9a493d0c809acb20da68fdf45
Kaushlendra Pratap <kaushlendra.pratap@corpseed.com>
2026-03-19 15:57:40 +0530

    company changes

--- src/main/java/com/doc/controller/company/CompanyController.java
+++ src/main/java/com/doc/controller/company/CompanyController.java
@@ -14,7 +14,7 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/operationService/operationService/api/companies")
+@RequestMapping("/operationService/api/companies")
 @Validated
 public class CompanyController {
 
