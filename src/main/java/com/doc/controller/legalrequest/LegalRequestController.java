cecb7cd8c9b62de40ac58304ade2e9db8b50a1c8
Kaushlendra Pratap <kaushlendra.pratap@corpseed.com>
2026-06-08 10:13:55 +0530

    change in operation prefix

--- src/main/java/com/doc/controller/legalrequest/LegalRequestController.java
+++ src/main/java/com/doc/controller/legalrequest/LegalRequestController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.time.LocalDateTime;
 
 @RestController
-@RequestMapping("/api/legal-requests")
+@RequestMapping("/operationService/api/legal-requests")
 public class LegalRequestController {
 
     private final LegalRequestService legalRequestService;
